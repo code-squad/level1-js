c02fb9954875021bbd3418216794c9ddbf125d0a
Honux Jung <honux77@gmail.com>
2017-10-31 12:12:56 +0900

    Add day7 Study

---
+++ day7/src/canvas.js
@@ -0,0 +1,10 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.strokeRect(0, 0, 640, 480);
+ctx.fillStyle = "Pink";
+ctx.fillRect(300, 220, 40, 40);
+ctx.strokeStyle = "AquaMarine";
+ctx.lineWidth = "5";
+ctx.strokeRect(290, 210, 60, 60);
+
+ctx.dr
