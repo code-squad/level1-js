a70b6f7d51c26483dcf2dfb1e9d9c166622a973e
Honux Jung <honux77@gmail.com>
2017-11-08 10:10:33 +0900

    Update sort data

--- lecture/day11/use-sort.js
+++ lecture/day11/use-sort.js
@@ -24,12 +24,14 @@ arr.length = 0;
 arr.push({name: "honux", score: 1000, money: 10});
 arr.push({name: "crong", score: 2000, money: 50});
 arr.push({name: "pobi1", score: 20, money: 20});
-arr.push({name: "pobi2", score: 40, money: 20});
 arr.push({name: "pobi3", score: 60, money: 20});
+arr.push({name: "pobi3", score: 30, money: 20});
+arr.push({name: "pobi2", score: 40, money: 20});
 
 console.log("before", arr);
 arr.sort((a,b) => (a.score - b.score));
 console.log("after", arr);
 arr.sort((a,b) => (a.money - b.money));
-console.log("Is it stable:", arr);
+console.log("Stable check:");
+console.log("after sort:", arr);
 
