286cd66dd1f502c339a3caaddecd767e3de7e6ff
Jina Kim <jinagold.k@gmail.com>
2017-11-20 10:28:29 +0900

    [Jina-2017.11.20]Update project Catrun (#61)
    * Jina first commit
    
    * Calculator 구현
    cal-css.css
    cal.html
    cal.js
    
    * Ryan 그리기
    ryan.html
    ryan.js
    
    * Second pull request test by hello.txt
    
    * Modified html script
    
    * JINA Project Catrun character img
    
    * JINA Project Catrun source html,css,js
    
    * JINA Project Catrun source file renamed catrun->index
    
    * Jina proejct Catrun update
    1. Bring the cat on the background
    2. Now the cat can move to the upside
    
    * Project Catrun update 2
    The cat can jump
    
    * Update project Catrun-moving background image
    
    * Update project Catrun
    Implement more features
    Now the background is moving.(but moving to the other side)

--- jina/catrun/index.js
+++ jina/catrun/index.js
@@ -7,7 +7,47 @@ catImage[0] = new Image();
 catImage[1] = new Image();
 catImage[0].src = "cat1.png";
 catImage[1].src = "cat2.png";
+//배경 부르기
+var bg = new Image();
+bg.src = 'bg.png';
+
+//장애물 변수
+var objW = 50;
+var objH = 50;
+
+//배경 변수
+var sizeX = 550;
+var sizeY = 350;
+var speed = 10;
+var yy = 0;
+var ddx = - 0.75;
+var xx = 0;
+//배경그리기
+function drawbg() {
+ 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	
+	ctx.drawImage(bg,xx - canvas.width,yy,canvas.width,canvas.height);
+
+ 		if (xx > sizeX) { xx = sizeX - canvas.width}
+ 		if (xx < sizeX + canvas.width) {
+ 			ctx.drawImage(bg,xx + canvas.width,yy,canvas.width,canvas.height);
+ 		}
+
+ 	ctx.drawImage(bg,xx,yy,canvas.width,canvas.height);
+
+ 	xx += ddx;
+}
+setInterval(drawbg, speed);
+
+//장애물 그리기
+function drawobj() {
+	ctx.beginPath();
+	ctx.rect(400,265,objW,objH);
+	ctx.fillStyle = "purple";
+	ctx.fill();
+	ctx.closePath();
+}
+
 //고양이 사이즈와 초기좌표
 var catwidth = 90;
 var catheight = 90;
@@ -21,12 +61,10 @@ var dy = -7;
 //고양이 중력
 var g = 0.2;
 
-//배경그리기	
-function drawbg() {
-	ctx.fillStyle = "lavender";
-	ctx.fillRect(10,10,canvas.width,canvas.height);
+//점프 실행 함수
+function jump() {
+	dy = -7;
 }
-
 
 //뛰는 고양이 그리기
 var count = 0;
@@ -36,7 +74,8 @@ var delay = 10;
 function drawcat() {
 	ctx.clearRect(0,0,canvas.width,canvas.height);
 	drawbg();
-	
+	drawobj();
+
 	catX += dx;
 	dy = dy + g;
 	catY += + dy;
@@ -44,8 +83,8 @@ function drawcat() {
 	if (catX >= 500 || catX <= 0) {
 		dx = -dx;
 	}
-	if(catY >= 200) {
-		catY = 200;
+	if(catY >= 230) {
+		catY = 230;
 	}
 
 	count++;
@@ -57,14 +96,12 @@ function drawcat() {
 	count = 0;
 	}
 
-	if (catY != 200) {
-		ctx.drawImage(catImage[0],catX,catY,catwidth,catheight);	
+	if (catY != 230) {
+		ctx.drawImage(catImage[0],catX,catY,catwidth,catheight);
 	} else {
-		ctx.drawImage(catImage[idx],catX,catY,catwidth,catheight);	
-
+		ctx.drawImage(catImage[idx],catX,catY,catwidth,catheight);
 	}
 	requestAnimationFrame(drawcat);
-	
 }
 
 requestAnimationFrame(drawcat);
