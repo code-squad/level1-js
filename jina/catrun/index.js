38a06be12fa9d95630fb1e7965bf2469b63714de
Jina Kim <jinagold.k@gmail.com>
2017-11-27 11:18:00 +0900

    [Jina]Update project Catrun (#63)
    * Jina first commit
    
    * Calculator 구현
    cal-css.css
    cal.html
    cal.js
    
    * Ryan 그리기
    ryan.html
    ryan.js
    
    * Second pull request test by hello.txt
    
    * Modified html script
    
    * JINA Project Catrun character img
    
    * JINA Project Catrun source html,css,js
    
    * JINA Project Catrun source file renamed catrun->index
    
    * Jina proejct Catrun update
    1. Bring the cat on the background
    2. Now the cat can move to the upside
    
    * Project Catrun update 2
    The cat can jump
    
    * Update project Catrun-moving background image
    
    * Update project Catrun
    Implement more features
    Now the background is moving.(but moving to the other side)
    
    * Update project Catrun
    Implement more features
    1. The background is moving from right to left.
    2. The cat is controlled with spacebar key.
    3. Made a obstacle(crab).
    4. Possible to collision detection
    
    * Add project Catrun's background
    
    * Add project Catrun's obstacle image
    
    * Update project Catrun
    Implement more feature
    Modify function collision-adjust scope of judgment
    
    * Update project Catrun
    Implement more feature
    Modify function collision-adjust scope of judgment
    
    * Update project Catrun
    Modify collision function

--- jina/catrun/index.js
+++ jina/catrun/index.js
@@ -7,45 +7,91 @@ catImage[0] = new Image();
 catImage[1] = new Image();
 catImage[0].src = "cat1.png";
 catImage[1].src = "cat2.png";
-//배경 부르기
+//배경 그림 부르기
 var bg = new Image();
+var bgLoad = false;
+bg.onload = function() {
+	bgLoad = true;
+}
 bg.src = 'bg.png';
 
+//장애물  그림 부르기
+var objImage = new Image();
+objImage.src = "obj.png";
 //장애물 변수
-var objW = 50;
-var objH = 50;
+var crab = {
+	"sx":35,
+	"sy":460,
+	"w":225,
+	"h":300,
+	"x":600,
+	"y":250,
+	"width":70,
+	"height":70
+};
+// function crab(x,width,height) {
+// 	this.sx = 35;
+// 	this.sy = 460;
+// 	this.w = 225;
+// 	this.h = 300;
+// 	this.x = 600;
+// 	this.y = 250;
+// 	this.width = 70;
+// 	this.height = 70;
+// }
+// crab.prototype.drawobj = function() {
+// 	ctx.drawImage(objImage,this.sx,this.sy,this.w,this.h
+// 		,this.x,this.y,this.width,this.height);
+// 		this.x -= 3;
+// }
+// function newobj(n) {
+// 	var width = random(30) + 20;
+// 	var x = new crab(650,60,60)
+// }
+//장애물 그리기
+function drawobj() {
+	ctx.drawImage(objImage,crab.sx,crab.sy,crab.w,crab.h
+		,crab.x,crab.y,crab.width,crab.height);
+		crab.x -= 3;
+}
+
+//장애물 충돌감지
+function collision() {
+
+	if (crab.x > catX-15 && crab.x < catX+catwidth-15
+	&& crab.y > catY-15 && crab.y < catY+catheight-15) {
+				debugger;
+				// alert("GAME OVER");
+				document.location.reload();
+	}
+}
+//점수 계산
+var score = 0;
+function checkScore() {
+	score++;
+	ctx.font = "16px Arial";
+	ctx.fillStyle = "#0095DD";
+	ctx.font = "20px Verdana";
+	ctx.fillText("Score: " + score, 15, 30);
+
+}
 
 //배경 변수
 var sizeX = 550;
 var sizeY = 350;
-var speed = 10;
-var yy = 0;
 var ddx = - 0.75;
-var xx = 0;
+var bgX = 0;
+var i = 0;
 //배경그리기
 function drawbg() {
- 	ctx.clearRect(0,0,canvas.width,canvas.height);
-	
-	ctx.drawImage(bg,xx - canvas.width,yy,canvas.width,canvas.height);
+	i=i+2;
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+	ctx.drawImage(bg,0 - i,0,sizeX, sizeY);
+	ctx.drawImage(bg,0 - i + sizeX,0,sizeX, sizeY);
 
- 		if (xx > sizeX) { xx = sizeX - canvas.width}
- 		if (xx < sizeX + canvas.width) {
- 			ctx.drawImage(bg,xx + canvas.width,yy,canvas.width,canvas.height);
- 		}
-
- 	ctx.drawImage(bg,xx,yy,canvas.width,canvas.height);
-
- 	xx += ddx;
-}
-setInterval(drawbg, speed);
-
-//장애물 그리기
-function drawobj() {
-	ctx.beginPath();
-	ctx.rect(400,265,objW,objH);
-	ctx.fillStyle = "purple";
-	ctx.fill();
-	ctx.closePath();
+	if (i == sizeX ) {
+		i = 0;
+	}
 }
 
 //고양이 사이즈와 초기좌표
@@ -53,32 +99,19 @@ var catwidth = 90;
 var catheight = 90;
 var catX = 100;
 var catY = 220;
-
 //고양이 움직임
 var dx = 0;
 var dy = -7;
-
 //고양이 중력
 var g = 0.2;
-
-//점프 실행 함수
-function jump() {
-	dy = -7;
-}
-
-//뛰는 고양이 그리기
+//고양이 그리기
 var count = 0;
 var idx = 0;
 var delay = 10;
-
 function drawcat() {
-	ctx.clearRect(0,0,canvas.width,canvas.height);
-	drawbg();
-	drawobj();
-
 	catX += dx;
 	dy = dy + g;
-	catY += + dy;
+	catY += dy;
 
 	if (catX >= 500 || catX <= 0) {
 		dx = -dx;
@@ -90,10 +123,10 @@ function drawcat() {
 	count++;
 	if (count >= delay) {
 		idx++;
-		if (idx > 1) {
-			idx = 0;
-		}
-	count = 0;
+	if (idx > 1) {
+		idx = 0;
+	}
+		count = 0;
 	}
 
 	if (catY != 230) {
@@ -101,7 +134,31 @@ function drawcat() {
 	} else {
 		ctx.drawImage(catImage[idx],catX,catY,catwidth,catheight);
 	}
-	requestAnimationFrame(drawcat);
 }
 
-requestAnimationFrame(drawcat);
+//키보드 조작
+document.addEventListener("keypress", jump);
+//점프 실행 함수
+function jump() {
+	if (event.keyCode == 32) {
+		if (catY > canvas.height / 2){
+			dy = -8;
+		}
+	}
+}
+
+//그리기
+
+function drawAll() {
+
+	ctx.clearRect(0,0,canvas.width,canvas.height);
+
+	drawbg();
+	drawobj();
+	collision();
+	drawcat();
+	checkScore();
+
+	requestAnimationFrame(drawAll);
+}
+requestAnimationFrame(drawAll);
