6d90f22c097669199190b6daa296f7586f83798d
JaeyeonKIM <foodsksms@gmail.com>
2017-12-29 15:02:35 +0900

    Jimmy second pull request (#75)
    [jimmy] Init and develop some ...
    
    * practice
    
    * 2017-12-29 commit message
    
    * Make deck

--- jimmy/game.js
+++ jimmy/game.js
@@ -1,10 +1,60 @@
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+// Decks
 
-var game = {
-  w : canvas.width,
-  h : canvas.height,
+function Game() {
+  this.currentTurnIndex = 0;
+  this.deck = new Deck();
 }
 
-ctx.fillColor = 'Red';
-ctx.fillRect(100, 100, game.w, game.h);
+function Deck() {
+  this.cards = [];
+  this.cardsDrawn = 0;
+  var shapes = ['spades', 'diamonds','hearts','clubs'];
+  var numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  for (var shape in shapes) {
+    for (var number of numbers) {
+      this.cards.push(new Card(numbers[number],shapes[shape]));
+    }
+  }
+}
+
+Deck.prototype.getCard = function () {
+  if (this.cards.length === this.cardsDrawn) {
+    return null;
+    // 아직 카드 안받을때
+  }
+  var random = Math.floor(Math.random() * (this.cards.length - this.cardsDrawn));
+  var temp = this.cards[random];
+
+  this.cards[random] = this.cards[this.cards.length - this.cardsDrawn - 1];
+  this.cards[this.cards.length - this.cardsDrawn - 1] = temp;
+  this.cardsDrawn++;
+
+  return temp;
+}
+
+function Card(number, shape) {
+  this.number = number;
+  this.shape = shape;
+}
+
+Card.prototype.value = function() {
+  if (this.number == "J" || "Q" || "K") {
+    return [0];
+  } else if (this.number = "A") {
+    return [1];
+  } else {
+    return Number(this.number, 10);
+  }
+};
+
+function Player() {
+  this.cards = [];
+}
+
+Player.prototype.addCard = function () {
+  this.cards.push(deck.getCard());
+};
+
+var deck = new Deck();
+var card = new Card();
+console.log(deck);
